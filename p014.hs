d9e5d4402aa40c3134b7641f4d6dd440d4db0bac
david.nemeth.csoka <david.nemeth.csoka@prezi.com>
2014-07-18 22:28:18 +0200

    p14 with dynamic programming

---
+++ p014.hs
@@ -0,0 +1,19 @@
+--n → n/2 (n is even)
+--n → 3n + 1 (n is odd)
+import Data.Array
+
+collatzLengths citem = r
+	where	
+		r = listArray (1, citem) (map collatz [1..citem])
+		memoized_collatz n
+			| n <= citem = r ! n
+			| otherwise  = collatz n
+		collatz n 
+			| n == 1 	= 1 
+			| even n 	= 1 + memoized_collatz (n `div` 2) 
+			| odd n 	= 1 + memoized_collatz (3 * n + 1)
+		
+
+maxSnd (a,b) (x,y) = if b < y then (x,y) else (a,b)
+
+solve = fst . foldr maxSnd (0,0) $ assocs (collatzLengths 1000000)
