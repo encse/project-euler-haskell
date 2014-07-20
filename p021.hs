eef654a8e714f1f82a91a3760976d24b680d7911
david.nemeth.csoka <david.nemeth.csoka@prezi.com>
2014-07-20 09:33:21 +0200

    p021 fast enought

--- p021.hs
+++ p021.hs
@@ -1,11 +1,38 @@
 import Data.Array
 
+isPrime :: Int -> Bool
+isPrime 1 = False
+isPrime 2 = True
+isPrime n | n `mod` 2 == 0 = False 
+isPrime n = 
+	let 
+		root = floor . sqrt . fromIntegral $ n  
+		lt = takeWhile (<= root) [3,5..]
+		notDivisor a = n `mod` a /= 0   
+	in
+		all notDivisor lt
+
 isDivisor a b = (a `mod` b) == 0
-properDivisors n = filter (isDivisor n) [1..((n `div` 2 )+ 1)]
 
-hasAmbientPair d n = 
-		(d ! n) /= n && (d ! (d ! n)) == n
+divisors n = filter (isDivisor n) [2..((n `div` 2 )+ 1)]
 
-solve = sum $ filter (hasAmbientPair d) [1..10000] 
-	where 
-		d = listArray (1,100000) (map (sum . properDivisors) [1..100000])
+smallestDivisor n = head $ divisors n
+
+multiplicityOfDivisor n p = maximum $ takeWhile (\x -> isDivisor n (p ^ x)) [1..] 
+
+d 1 = 0
+d n 
+	| isPrime n	= 1
+	| otherwise =
+		let 
+			p = smallestDivisor n
+			m = multiplicityOfDivisor n p
+			s = sum $ map (p ^ ) [0..m]
+			rem = n `div` (p ^ m)
+		in
+			((d rem) + rem) * s - n
+
+hasAmbientPair n = 
+		(d n) /= n && (d (d n)) == n
+
+solve = sum $ filter (hasAmbientPair) [2..10000] 
